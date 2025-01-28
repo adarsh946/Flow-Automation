3489d03ea52bf4cb2dc24c63d0ca1136f1b67496
adarsh946 <adarsh.shukla2724@gmail.com>
2025-01-28 23:01:29 +0530

    added processor logic

---
+++ apps/processor/index.ts
@@ -0,0 +1,43 @@
+import { Kafka } from "kafkajs";
+import client from "@repo/db";
+
+const TOPIC_NAME = "zap-events";
+const kafka = new Kafka({
+  clientId: "processor",
+  brokers: ["localhost:9092"],
+});
+
+const main = async () => {
+  const producer = kafka.producer();
+  await producer.connect();
+
+  while (1) {
+    const pendingTasks = await client.zapRunOutBox.findMany({
+      where: {},
+      take: 10,
+    });
+
+    console.log(pendingTasks);
+
+    await producer.send({
+      topic: TOPIC_NAME,
+      messages: pendingTasks.map((r) => {
+        return {
+          value: JSON.stringify({ zapRunId: r.zapRunId, stage: 0 }),
+        };
+      }),
+    });
+
+    await client.zapRunOutBox.deleteMany({
+      where: {
+        id: {
+          in: pendingTasks.map((x) => x.id),
+        },
+      },
+    });
+
+    await new Promise((r) => setTimeout(r, 3000));
+  }
+};
+
+main();

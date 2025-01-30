26a945893b0b0513af5c321b5012275f4913e114
adarsh946 <adarsh.shukla2724@gmail.com>
2025-01-30 22:20:16 +0530

    basic frontend nextjs setup

---
+++ packages/ui/src/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <div className="">
+        <img
+          src="https://cdn.freelogovectors.net/wp-content/uploads/2023/11/make-logo-freelogovectors.net_.png"
+          alt="logo"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
